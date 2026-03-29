9bd20a670a21ae1f07cf17440c7461db462d6a2e
Sakshikain7078 <kainsakshi56@gmail.com>
2026-03-29 19:54:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.85%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -3,5 +3,5 @@ class Solution:
         s1_even = sorted([s1[0],s1[2]])
         s1_odd = sorted([s1[1],s1[3]])
         s2_even = sorted([s2[0],s2[2]])
-        s2_odd = sorted([s2[1],s2[3]])
-        return s1_even == s2_even and s1_odd == s2_odd
+        s2_odd = sorted([s2[1],s2[3]])    
+        return s1_even  == s2_even and s2_odd == s1_odd
