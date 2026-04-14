f8eb783698816016b56e1f6f6a282dd899e39ffe
Sakshikain7078 <kainsakshi56@gmail.com>
2026-04-14 18:02:43 +0530

    Time: 495 ms (74.07%) | Memory: 135.7 MB (17.28%) - LeetSync

---
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.py
@@ -0,0 +1,28 @@
+from scipy.optimize import linprog
+import numpy as np
+
+class Solution:
+    def minimumTotalDistance(self, robot, factory):
+        n = len(robot)
+        m = len(factory)
+    
+        costs = []
+        for r in robot:
+            for f_pos, limit in factory:
+                costs.append(abs(r - f_pos))
+            
+        A_eq = np.zeros((n, n * m))
+        for i in range(n):
+            A_eq[i, i*m : (i+1)*m] = 1
+        b_eq = np.ones(n)
+    
+        
+        A_ub = np.zeros((m, n * m))
+        for j in range(m):
+            for i in range(n):
+                A_ub[j, i*m + j] = 1
+        b_ub = [f[1] for f in factory]
+    
+        res = linprog(costs, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, method='highs')
+    
+        return int(round(res.fun))        
