b4783b2e637512a5005255aae09ea2ac5baa5d24
Sakshikain7078 <kainsakshi56@gmail.com>
2026-05-16 12:31:35 +0530

    Time: 3 ms (83.56%) | Memory: 19.4 MB (19.29%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
@@ -3,20 +3,19 @@ class Solution:
         neg = []
         pos = []
         for num in nums:
-            if num < 0:
+            if num<0:
                 neg.append(num)
             else:
                 pos.append(num)
         if not neg:
             return min(pos)
-        elif not pos:
-            return max(neg)  
+        elif  not pos:
+            return max(neg)
         else:
             a = max(neg)
             b = min(pos)
-            if abs(a) < abs(b):
+            if abs(a)<abs(b):
                 return a
-            elif abs(b) < abs(a):
+            elif abs(b)<abs(a):
                 return b
-            else:
-                return max(a,b)     
+            else: return max(a,b)
